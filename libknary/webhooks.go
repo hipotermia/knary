64162a4a34059ae9e588b43454287b67046b6163
hipotermia <46414327+hipotermia@users.noreply.github.com>
2021-03-01 15:04:31 +0100

    Update webhooks.go

--- libknary/webhooks.go
+++ libknary/webhooks.go
@@ -106,7 +106,7 @@ func sendMsg(msg string) {
 		}
 	}
 	
-	if os.Getenv("TELEGRAM_TOKEN") != "" {
+	if os.Getenv("TELEGRAM_CHAT_ID") != "" && os.Getenv("TELEGRAM_TOKEN") != "" {
 		_, err := http.PostForm("https://api.telegram.org/bot" + os.Getenv("TELEGRAM_TOKEN") + "/sendMessage", url.Values{"chat_id": {os.Getenv("TELEGRAM_CHAT_ID")}, "text": {msg}})
 
 		if err != nil {
